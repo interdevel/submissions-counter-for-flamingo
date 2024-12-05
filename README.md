4e7e9c3c075e325bb0dd6624a16d5c09867a0d27
Luis Molina <interdevel@gmail.com>
2024-12-05 14:45:28 +0100

    Added: plugin main file and README

---
+++ README.md
@@ -0,0 +1,32 @@
+=== Flamingo Counter ===
+
+Contributors: interdevel  
+Tags: flamingo, cf7, shortcode
+Requires at least: 5.3  
+Tested up to: 6.7 
+Requires PHP: 7.4  
+Stable tag: 1.0.0  
+License: GPL v2 or later  
+License URI: https://www.gnu.org/licenses/gpl-2.0.html  
+
+Provides a shortcode to show the number of form submissions stored by Flamingo & Contact Form 7.
+
+== Description ==
+
+The **Flamingo Counter** provides a simple shortcode that you can use in any page or post to show how many submissions are stored in forms created with Contact Form 7.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/flamingo-counter` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+3. Use the shortcode as any other WordPress shortcode.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release of the Flamingo Counter plugin.
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+Initial release.
---
+++ flamingo-simple-counter.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Plugin Name: Flamingo Counter
+ * Description: A shortcode to show the number of form submissions stored by Flamingo & Contact Form 7.
+ * Version: 1.0.0
+ * Author: Luis Molina
+ * Tags: flamingo, cf7, shortcode
+ * License: GPLv2 or later
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Returns the number of submissions stored by Flamingo for one form
+ */
+function fsc_count_flamingo_submissions($atts) {
+
+    // Shortcode attributes
+    $atts = shortcode_atts(
+        array(
+            'form_title' => '', // Form title (optional)
+            'html_tag'   => '', // HTML tag to wrap the number (optional)
+        ),
+        $atts
+    );
+
+    $form_title = sanitize_text_field($atts['form_title']);
+    $q = false;
+
+    if ( ! empty($form_title) ) {
+
+        // Count submissions for a specific form
+        $count = new WP_Query(array(
+            'post_type' => 'flamingo_inbound',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'flamingo_inbound_channel',
+                    'field'    => 'name',
+                    'terms'    => $form_title,
+                ),
+            ),
+            'fields' => 'ids', // Just count
+        ));
+
+        $q = $count->found_posts ?: 0;
+
+    } else {
+
+        // Count all submissions
+        $count = wp_count_posts('flamingo_inbound');
+        $q = $count->publish ?: 0;
+
+    }
+
+    if (false !== $q) {
+        $htmltag = sanitize_text_field($atts['html_tag']);
+        if ( empty($htmltag)) {
+            return $q;
+        } else {
+            return "<$htmltag class=\"fsc-count\">$q</$htmltag>";
+        }
+    }
+
+}
+add_shortcode('flamingo_count', 'fsc_count_flamingo_submissions');
+
